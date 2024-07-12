c23c5e7e80a78d016320022938ed5ce134e34729
Adebisijoshua <Adekunle8k@gmail.com>
2024-07-12 16:21:09 -0400

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,4 @@
-# This manifest installs Flask version 2.1.0 using pip3
-
-# Install pip3 package
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep Version | grep -q 2.1.0',
-  require => Package['python3-pip'],
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
